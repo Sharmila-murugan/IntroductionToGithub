af512b74a68031e57ec875281b11010c7fdd4db7
Sharmila Murugan <Sharmila.murugan@ilink-systems.com>
2023-11-21 19:06:12 +0530

    learn python

---
+++ main1.py
@@ -0,0 +1,8 @@
+n=int(input("Enter the value :"))
+
+print(n)
+print("line 2 is to be committed")
+print("Coding is fun.......")
+
+print("Welcome Back Let's Learn Python with simple code")
+print("Hello World !!!!!!!!")
